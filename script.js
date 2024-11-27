8c981ce1179edd95aab332eb2c0b3a0d80470916
Modokilloko <arthur.davies13@gmail.com>
2024-11-27 15:50:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// This JavaScript file can be expanded in the future
+// For now, it is empty, but you can add interactivity like form validation, or booking system
+console.log('Self-Drive Tours Mongolia Website Loaded');
